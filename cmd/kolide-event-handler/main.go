d387eaf8b8edccaa0e59e9e988a598f369c595a8
Christer Edvartsen <christer.edvartsen@nav.no>
2021-03-12 09:40:39 +0100

    Remove TLS

--- cmd/kolide-event-handler/main.go
+++ cmd/kolide-event-handler/main.go
@@ -10,7 +10,6 @@ import (
 	"syscall"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 
 	keh "github.com/nais/kolide-event-handler/pkg/kolide-event-handler"
 	kehs "github.com/nais/kolide-event-handler/pkg/kolide-event-handler-server"
@@ -22,17 +21,11 @@ import (
 var (
 	kolideSigningSecret string
 	kolideApiToken      string
-	tlsKeyPath          string
-	tlsCertPath         string
-	tlsCAPath           string
 )
 
 func init() {
 	flag.StringVar(&kolideSigningSecret, "kolide-signing-secret", os.Getenv("KOLIDE_SIGNING_SECRET"), "Secret for verifying webhook payloads from Kolide")
 	flag.StringVar(&kolideApiToken, "kolide-api-token", os.Getenv("KOLIDE_API_TOKEN"), "API token for the Kolide API")
-	flag.StringVar(&tlsCertPath, "tls-cert", "/var/run/secrets/tls/tls.crt", "Server certificate path")
-	flag.StringVar(&tlsKeyPath, "tls-key", "/var/run/secrets/tls/tls.key", "Server key path")
-	flag.StringVar(&tlsCAPath, "tls-ca", "/var/run/secrets/ca/ca.crt", "Client CA path")
 	flag.Parse()
 }
 
@@ -64,12 +57,6 @@ func main() {
 
 	go startHttpServer(httpListener, handler.Routes())
 
-	creds, err := loadTLS(tlsCertPath, tlsKeyPath, tlsCAPath)
-	if err != nil {
-		log.Errorf("reading credentials from file: %v", err)
-		return
-	}
-
 	grpcListener, err := net.Listen("tcp", "127.0.0.1:8081")
 
 	if err != nil {
@@ -78,7 +65,7 @@ func main() {
 	}
 
 	server := kehs.New(deviceListChan)
-	go startGrpcServer(grpcListener, server, creds)
+	go startGrpcServer(grpcListener, server)
 
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
@@ -86,12 +73,8 @@ func main() {
 	log.Infof("Received %s, shutting down gracefully.", sig)
 }
 
-func startGrpcServer(listener net.Listener, server kehs.KolideEventHandlerServer, creds credentials.TransportCredentials) {
-	grpcServer := grpc.NewServer(
-		grpc.Creds(creds),
-		grpc.UnaryInterceptor(LogClientCertSubj),
-		grpc.StreamInterceptor(StreamLogClientCertSubj),
-	)
+func startGrpcServer(listener net.Listener, server kehs.KolideEventHandlerServer) {
+	grpcServer := grpc.NewServer()
 
 	pb.RegisterKolideEventHandlerServer(grpcServer, server)
 
--- cmd/test-client/main.go
+++ cmd/test-client/main.go
@@ -2,45 +2,25 @@ package main
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
 	"flag"
-	"fmt"
-	"io/ioutil"
 
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
 
 	"github.com/nais/kolide-event-handler/pkg/pb"
 )
 
 var (
 	server      string
-	TLSKeyPath  string
-	TLSCertPath string
-	TLSCAPath   string
-	TLSExtraPath   string
 )
 
 func init() {
 	flag.StringVar(&server, "server", "127.0.0.1:8081", "target grpc server")
-	flag.StringVar(&TLSCertPath, "tls-cert", "/var/run/secrets/tls/tls.crt", "Server certificate path")
-	flag.StringVar(&TLSKeyPath, "tls-key", "/var/run/secrets/tls/tls.key", "Server key path")
-	flag.StringVar(&TLSCAPath, "tls-ca", "/var/run/secrets/ca/ca.crt", "Client CA path")
-	flag.StringVar(&TLSExtraPath, "tls-extra", "/var/run/secrets/ca/extra.crt", "Client extra path")
 	flag.Parse()
 }
 
 func main() {
-	creds, err := loadTLS(TLSCertPath, TLSKeyPath, TLSCAPath, TLSExtraPath)
-	if err != nil {
-		log.Errorf("loading tls: %v", err)
-		return
-	}
-
-	conn, err := grpc.Dial(server, grpc.WithTransportCredentials(creds))
-	//conn, err := grpc.Dial(server, grpc.WithInsecure())
+	conn, err := grpc.Dial(server, grpc.WithInsecure())
 	if err != nil {
 		log.Errorf("connecting to grpc server: %v", err)
 	}
@@ -69,37 +49,4 @@ func main() {
 
 		log.Infof("event received: %+v", event)
 	}
-}
-
-func loadTLS(certPath, keyPath, caPath, extraPath string) (credentials.TransportCredentials, error) {
-	certificate, err := tls.LoadX509KeyPair(certPath, keyPath)
-	if err != nil {
-		return nil, fmt.Errorf("loading keypair: %w", err)
-	}
-
-	ca, err := ioutil.ReadFile(caPath)
-	if err != nil {
-		return nil, fmt.Errorf("readin ca file: %w", err)
-	}
-
-	caPool := x509.NewCertPool()
-
-	if !caPool.AppendCertsFromPEM(ca) {
-		return nil, fmt.Errorf("unable to add ca to ca pool")
-	}
-
-	extra, err := ioutil.ReadFile(extraPath)
-	if err != nil {
-		return nil, fmt.Errorf("readin ca file: %w", err)
-	}
-	if !caPool.AppendCertsFromPEM(extra) {
-		return nil, fmt.Errorf("unable to add extra to ca pool")
-	}
-
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{certificate},
-		RootCAs:      caPool,
-	}
-
-	return credentials.NewTLS(tlsConfig), nil
 }
