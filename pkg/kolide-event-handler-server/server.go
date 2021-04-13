4be08dfcd3e91a95cc751b720bfc069c72ec618c
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2021-04-13 13:10:10 +0200

    message
    Co-authored-by: Christer Edvartsen <Christer.Edvartsen@nav.no>feature: handle multiple connected clients

--- pkg/kolide-event-handler-server/server.go
+++ pkg/kolide-event-handler-server/server.go
@@ -2,8 +2,6 @@ package kolide_event_handler_server
 
 import (
 	"context"
-	"fmt"
-
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,16 +16,54 @@ func New(ctx context.Context, deviceListChan <-chan *pb.DeviceList) KolideEventH
 	}
 }
 
+func (kehs *kolideEventHandlerServer) newDeviceListReceiver() (<-chan *pb.DeviceList, int) {
+	kehs.channelOperationLock.Lock()
+	defer kehs.channelOperationLock.Unlock()
+
+	n := kehs.channelIDCounter
+	kehs.channelIDCounter += 1
+
+	deviceListChan := make(chan *pb.DeviceList, 50)
+	kehs.deviceListReceivers[n] = deviceListChan
+
+	return deviceListChan, n
+}
+
+func (kehs *kolideEventHandlerServer) deleteDeviceListReceiver(n int) () {
+	kehs.channelOperationLock.Lock()
+	defer kehs.channelOperationLock.Unlock()
+
+	delete(kehs.deviceListReceivers, n)
+}
+
+func (kehs *kolideEventHandlerServer) broadcastDeviceList(deviceList *pb.DeviceList) {
+	kehs.channelOperationLock.Lock()
+	defer kehs.channelOperationLock.Unlock()
+
+	for _, c := range kehs.deviceListReceivers {
+		c <- deviceList
+	}
+}
+
 func (kehs *kolideEventHandlerServer) Events(request *pb.EventsRequest, server pb.KolideEventHandler_EventsServer) error {
+	deviceListReceiver, n := kehs.newDeviceListReceiver()
+
 	for {
 		select {
-		case deviceList := <-kehs.deviceListChan:
+		case deviceList := <-deviceListReceiver:
 			err := server.Send(deviceList)
 
 			if err != nil {
-				return fmt.Errorf("sending device list: %w", err)
+				return status.Errorf(status.Code(err), "sending device list: %v", err)
 			}
+
+		case <-server.Context().Done():
+			kehs.deleteDeviceListReceiver(n)
+			log.Info("Events request done")
+			return status.Errorf(codes.Canceled, "Events request done")
+
 		case <-kehs.ctx.Done():
+			kehs.deleteDeviceListReceiver(n)
 			log.Info("kolide event handler context cancelled")
 			return status.Errorf(codes.Unavailable, "server shutting down")
 		}
--- pkg/kolide-event-handler-server/types.go
+++ pkg/kolide-event-handler-server/types.go
@@ -2,6 +2,7 @@ package kolide_event_handler_server
 
 import (
 	"context"
+	"sync"
 
 	"github.com/nais/kolide-event-handler/pkg/pb"
 )
@@ -10,6 +11,10 @@ type kolideEventHandlerServer struct {
 	pb.UnimplementedKolideEventHandlerServer
 	deviceListChan <-chan *pb.DeviceList
 	ctx            context.Context
+
+	deviceListReceivers  map[int]chan<- *pb.DeviceList
+	channelIDCounter     int
+	channelOperationLock sync.Mutex
 }
 
 type KolideEventHandlerServer interface {
